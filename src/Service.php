9221c6c72ab1b67667ae1e695794e4eb68cc6310
aspirantzhang <admin@aspirantzhang.com>
2021-09-11 16:15:54 +0800

    feat: add service

---
+++ src/Service.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace aspirantzhang\octopusRevision;
+
+class Service extends \think\Service
+{
+    public function boot()
+    {
+        $this->app->bind('revision', Revision::class);
+    }
+}
