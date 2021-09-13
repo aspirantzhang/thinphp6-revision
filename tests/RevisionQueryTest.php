a523f032a468a8c865d20c5b7d6cebbf477df3d3
aspirantzhang <admin@aspirantzhang.com>
2021-09-13 15:32:26 +0800

    feat: add revision query

---
+++ tests/RevisionQueryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace aspirantzhang\octopusRevision;
+
+use think\facade\Db;
+
+class RevisionQueryTest extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        Db::execute('DROP TABLE IF EXISTS `revision`');
+        Db::execute(<<<END
+CREATE TABLE `revision` (
+ `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+ `table_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+ `original_id` int(11) unsigned NOT NULL DEFAULT 0,
+ `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+ `main_data` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+ `i18n_data` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+ `create_time` datetime NOT NULL,
+ `update_time` datetime NOT NULL,
+ `delete_time` datetime DEFAULT NULL,
+ `status` tinyint(1) NOT NULL DEFAULT 1,
+ PRIMARY KEY (`id`),
+ KEY `table_name` (`table_name`,`original_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+END
+        );
+        $time = '2001-01-01 01:01:01';
+        $moreRecords = [];
+        for ($i = 0; $i < 20; $i++) {
+            $moreRecords[] = [
+                'table_name' => 'revision_test',
+                'original_id' => 100,
+                'title' => 'revision_test_' . $i,
+                'main_data' => '[]',
+                'i18n_data' => '[]',
+                'create_time' => $time,
+                'update_time' => $time,
+            ];
+        }
+        Db::name('revision')->insertAll($moreRecords);
+    }
+
+    public function testRevisionQueryDefaultPage()
+    {
+        $result = (new RevisionQuery())->query();
+        $this->assertStringStartsWith('[{"id":20,"table_name":"revision_test","original_id":100,"title":"revision_test_19', json_encode($result));
+    }
+    public function testRevisionQuerySpecificPageAndPerPage()
+    {
+        $result = (new RevisionQuery())->query(3, 2);
+        $this->assertEquals(count($result), 2);
+        $this->assertStringStartsWith('[{"id":16', json_encode($result));
+    }
+}
