e1628ce64a1bf2de6cc007068c1050baae024a93
aspirantzhang <admin@aspirantzhang.com>
2021-09-13 16:51:12 +0800

    fix: fix empty check condition logic in query

--- src/RevisionQuery.php
+++ src/RevisionQuery.php
@@ -10,7 +10,7 @@ class RevisionQuery
 {
     public function query(string $tableName, int $recordId, int $page = 1, $perPage = 5)
     {
-        if (empty($tableName) && empty($recordId)) {
+        if (empty($tableName) || empty($recordId)) {
             throw new \InvalidArgumentException('Table name and record id should not be empty.');
         }
         $list = Db::name('revision')
